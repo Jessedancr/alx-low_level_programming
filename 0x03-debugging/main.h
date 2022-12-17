2da431f43e84da929c1a21fcc8fde9a9e13d0be7
Jesse Ikemfuna <jesseikemefuna@gmail.com>
2022-12-16 19:32:08 -0800

    task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-postitive_or_negative(int i);
+void postitive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
