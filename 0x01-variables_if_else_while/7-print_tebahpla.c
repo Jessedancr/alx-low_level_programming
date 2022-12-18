334db667329cd5684fa2e18fa780924b9a30f000
Jesse Ikemfuna <jesseikemefuna@gmail.com>
2022-12-18 12:57:30 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print if the number is postive negative or zero
+ * Descrption: using the main function
+ * this program prints blah blah blha
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
