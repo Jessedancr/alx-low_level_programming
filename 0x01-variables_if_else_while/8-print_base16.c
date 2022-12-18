8c0f50a448a700db50256a4533c769ecc5ae5851
Jesse Ikemfuna <jesseikemefuna@gmail.com>
2022-12-18 13:02:03 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive negative or zero
+ * Description: using the main function
+ * this program prins moni fuck
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
