1acf3b20549548fdc085631ccbf772a36a106ef4
Jesse Ikemfuna <jesseikemefuna@gmail.com>
2022-12-18 08:26:47 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive negative or zero
+ * Description: using the main functon
+ * this program prints blah blah blah
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	for (x = 0; x <= 9; x++)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}
