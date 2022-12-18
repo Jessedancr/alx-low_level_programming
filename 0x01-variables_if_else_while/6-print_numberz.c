3005c299c37f05f302c5bcc4b39478d597869c48
Jesse Ikemfuna <jesseikemefuna@gmail.com>
2022-12-18 08:31:13 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive negative or zero
+ * Description: using the mai function for
+ * this program prints blah blah blah
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= 9; ch++)
+		putchar(ch);
+	putchar("\n");
+	return (0);
+}
