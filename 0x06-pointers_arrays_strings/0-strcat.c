025f2a3399ff997b23cce76291b5ef383b626982
Jesse Ikemfuna <jesseikemefuna@gmail.com>
2022-12-21 15:31:02 -0800

    main.h readme file and task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * strcat - concatenates the string pointed to by @src
+ * @dest: a pointer to the string to be concatenated upon
+ * @src: the source string to be appended to by @dest
+ * Return: a pointer to the destination string @dest
+ */
+char *strcat(char *dest, const char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+more on arrays pointers and strings
