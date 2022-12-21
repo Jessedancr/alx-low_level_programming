68f53d4f5b5f842af4213f0d231127e7e3897fab
Jesse Ikemfuna <jesseikemefuna@gmail.com>
2022-12-20 18:23:11 -0800

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _atoi - converts string to an integer
+ * @s: pointer to a character string
+ * Return: void
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+
+	unsigned int num = 0;
+
+	char *temp = s;
+
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++
+	}
+	if (*temp != '\0')
+	{
+		do{
+			num = num* 10 + (*temp - '0');
+			temp++;
+		}while (*temp >='0' && *temp <= '9');
+	}return (num * sign);
+}
