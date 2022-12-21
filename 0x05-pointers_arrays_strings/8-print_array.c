2f1123470acc66bb743347f6d33198281e210e34
Jesse Ikemfuna <jesseikemefuna@gmail.com>
2022-12-20 18:05:36 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,12 +7,12 @@
  */
 void print_array(int *a, int n)
 {
-	int t;
+	int i;
 
-	for (t = 0; t < n; t++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[t]);
-		if (t != (n - t))
+		printf("%d", a[i]);
+		if (i != n - 1)
 		{
 			printf(", ");
 			printf(" ");
