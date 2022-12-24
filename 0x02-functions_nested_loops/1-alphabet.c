d74f7a5996178eef686902eaf9b2bf73fca69aaa
Jesse Ikemfuna <jesseikemefuna@gmail.com>
2022-12-24 15:28:26 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabets in lower case
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		_putchar(letter);
+}
